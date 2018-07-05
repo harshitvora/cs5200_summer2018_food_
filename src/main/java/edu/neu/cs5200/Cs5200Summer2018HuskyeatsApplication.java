99944b767e99f4d775588e9188ba5b753f35a96f
aayushi <maheshwari.aa@husky.neu.edu>
2018-07-05 18:06:44 -0400

    adding initial files

---
+++ src/main/java/edu/neu/cs5200/Cs5200Summer2018HuskyeatsApplication.java
@@ -0,0 +1,12 @@
+package edu.neu.cs5200;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs5200Summer2018HuskyeatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs5200Summer2018HuskyeatsApplication.class, args);
+	}
+}
