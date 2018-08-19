cdefb46cb432d6faa06b2cb2e509a8a423a4aa61
harshitvora <harshit.hvora@gmail.com>
2018-08-19 11:07:46 -0400

    Removed react

--- myapp/services/RestaurantService.js
+++
@@ -1,8 +0,0 @@
-const RESTAURANT_API_URL = 'http://10.0.0.182:8080/api/restaurant';
-
-export function findAllRestaurants() {
-    return fetch(RESTAURANT_API_URL)
-        .then(function (response) {
-            return response.json();
-        });
-}
